b44e1e08e3ff565fd916847c8a5d0723e9500b22
kufikugel <kufikugel@googlemail.com>
2017-10-26 16:50:32 +0530

    Safe headset volume option (2 of 2) Add switch for Safe headset volume.

--- src/com/delight/settings/VolumeRockerSettings.java
+++ src/com/delight/settings/VolumeRockerSettings.java
@@ -15,13 +15,19 @@
  */
 package com.delight.settings;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.app.Fragment;
 import android.content.ContentResolver;
+import android.content.DialogInterface;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.os.UserHandle;
 import android.support.v7.preference.ListPreference;
 import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceScreen;
+import android.support.v14.preference.SwitchPreference;
 import android.provider.Settings;
 
 import com.android.internal.logging.nano.MetricsProto;
@@ -34,9 +40,12 @@ public class VolumeRockerSettings extends SettingsPreferenceFragment implements
 
     private static final String HEADSET_CONNECT_PLAYER = "headset_connect_player";
     private static final String RINGTONE_FOCUS_MODE = "ringtone_focus_mode";
+    private static final int DLG_SAFE_HEADSET_VOLUME = 0;
+    private static final String KEY_SAFE_HEADSET_VOLUME = "safe_headset_volume";
 
     private ListPreference mLaunchPlayerHeadsetConnection;
     private ListPreference mHeadsetRingtoneFocus;
+    private SwitchPreference mSafeHeadsetVolume;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -57,6 +66,11 @@ public class VolumeRockerSettings extends SettingsPreferenceFragment implements
         mHeadsetRingtoneFocus.setValue(Integer.toString(mHeadsetRingtoneFocusValue));
         mHeadsetRingtoneFocus.setSummary(mHeadsetRingtoneFocus.getEntry());
         mHeadsetRingtoneFocus.setOnPreferenceChangeListener(this);
+
+        mSafeHeadsetVolume = (SwitchPreference) findPreference(KEY_SAFE_HEADSET_VOLUME);
+        mSafeHeadsetVolume.setChecked(Settings.System.getInt(resolver,
+				Settings.System.SAFE_HEADSET_VOLUME, 1) != 0);
+        mSafeHeadsetVolume.setOnPreferenceChangeListener(this);
     }
 
     @Override
@@ -65,6 +79,7 @@ public class VolumeRockerSettings extends SettingsPreferenceFragment implements
     }
 
     public boolean onPreferenceChange(Preference preference, Object newValue) {
+        final String key = preference.getKey();
         ContentResolver resolver = getActivity().getContentResolver();
         if (preference == mLaunchPlayerHeadsetConnection) {
             int mLaunchPlayerHeadsetConnectionValue = Integer.valueOf((String) newValue);
@@ -74,6 +89,13 @@ public class VolumeRockerSettings extends SettingsPreferenceFragment implements
             Settings.System.putIntForUser(resolver, Settings.System.HEADSET_CONNECT_PLAYER,
                     mLaunchPlayerHeadsetConnectionValue, UserHandle.USER_CURRENT);
             return true;
+        } else if (KEY_SAFE_HEADSET_VOLUME.equals(key)) {
+            if ((Boolean) newValue) {
+                    Settings.System.putInt(getActivity().getContentResolver(),
+                            Settings.System.SAFE_HEADSET_VOLUME, 1);
+            } else {
+                    showDialogInner(DLG_SAFE_HEADSET_VOLUME);
+            }
         } else if (preference == mHeadsetRingtoneFocus) {
             int mHeadsetRingtoneFocusValue = Integer.valueOf((String) newValue);
             int index = mHeadsetRingtoneFocus.findIndexOfValue((String) newValue);
@@ -84,5 +106,63 @@ public class VolumeRockerSettings extends SettingsPreferenceFragment implements
             return true;
         }
         return false;
+    }
+
+
+    private void showDialogInner(int id) {
+        DialogFragment newFragment = MyAlertDialogFragment.newInstance(id);
+        newFragment.setTargetFragment(this, 0);
+        newFragment.show(getFragmentManager(), "dialog " + id);
+    }
+
+    public static class MyAlertDialogFragment extends DialogFragment {
+
+        public static MyAlertDialogFragment newInstance(int id) {
+            MyAlertDialogFragment frag = new MyAlertDialogFragment();
+            Bundle args = new Bundle();
+            args.putInt("id", id);
+            frag.setArguments(args);
+            return frag;
+        }
+
+        VolumeRockerSettings getOwner() {
+            return (VolumeRockerSettings) getTargetFragment();
+        }
+
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            int id = getArguments().getInt("id");
+                switch (id) {
+                   case DLG_SAFE_HEADSET_VOLUME:
+                       return new AlertDialog.Builder(getActivity())
+                           .setTitle(R.string.attention)
+                           .setMessage(R.string.safe_headset_volume_warning_dialog_text)
+                           .setPositiveButton(R.string.ok,
+                               new DialogInterface.OnClickListener() {
+                                   public void onClick(DialogInterface dialog, int which) {
+                                       Settings.System.putInt(getOwner().getActivity().getContentResolver(),
+                                       Settings.System.SAFE_HEADSET_VOLUME, 0);
+                                   }
+                               })
+                           .setNegativeButton(R.string.cancel,
+                               new DialogInterface.OnClickListener() {
+                                   public void onClick(DialogInterface dialog, int which) {
+                                       dialog.cancel();
+                                   }
+                               })
+                           .create();
+                }
+                   throw new IllegalArgumentException("unknown id " + id);
+        }
+
+        @Override
+        public void onCancel(DialogInterface dialog) {
+            int id = getArguments().getInt("id");
+                switch (id) {
+                   case DLG_SAFE_HEADSET_VOLUME:
+                       getOwner().mSafeHeadsetVolume.setChecked(true);
+                       break;
+                   }
+        }
     }
 }
